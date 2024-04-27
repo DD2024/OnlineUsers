cb70db184ec563aa453423289176f0e2a7c30555
Dmitriy <dmdperm@yandex.ru>
2024-04-27 16:25:44 +0500

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("был(а) " + agoToText(18000))
+    println("был(а) " + agoToText(70000))
 }
 
 fun agoToText(time: Int): String{
@@ -7,10 +7,12 @@ fun agoToText(time: Int): String{
     val minutesToString = (time/60).toString();
     val hoursToString = (time/3600).toString();
     val activeTime: String
-    when(time){
-
-        in 1..60 -> activeTime = "только что"
-        in 61..60 * 59 ->  activeTime =  getFormMinutes(time / 60, minutesToString) + " назад"
+    when{
+        time in 1..60 -> activeTime = "только что"
+        time in 61..60 * 59 ->  activeTime =  getFormMinutes(time / 60, minutesToString) + " назад"
+        time in 90000 .. 172800 -> activeTime = "вчера"
+        time in 172801 .. 259200 -> activeTime = "позавчера"
+        time > 259201 -> activeTime = "давно"
         else -> activeTime = getFormHours (time / 3600, hoursToString) + " назад"
     }
     return activeTime
@@ -26,5 +28,6 @@ fun getFormMinutes(minutes: Int, minutesToString: String) = when(minutes){
 fun getFormHours(hours: Int, hoursToString: String) = when(hours) {
     1, 21 -> hoursToString + " час"
     2, 3, 4, 22, 23, 24 -> hoursToString + " часа"
+
     else -> hoursToString + " часов"
 }
